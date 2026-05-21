24d76748c4d3b4a3ad3da23a6d9ff16ea59ee73f
Qasimovich <shah.qasim03@gmail.com>
2026-05-21 21:24:19 +0200

    Quartz sync: May 21, 2026, 9:24 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "qasimovich.github.io/Islam-101",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
